deb9e7ec0909ef04923985e5bc8d22cda094ff45
CRYPTOMASTER09 <technicalakmal225@gmail.com>
2024-12-06 17:09:39 +0500

    main.php

---
+++ main.php
@@ -0,0 +1,31 @@
+_w-2x?wzsn5MֹoF]9֛ƚ-.춳?vڱלוS^{?u(u672צmyy~ܶ*',y._;^]=s5MtӶN}׾'{nyzmѾow7}nq}t۷|}ty޸~4o=kMiߺw-2)?kN5VuǸmi5munq}t۷|}ty޸~4o=k!MYۜr|ǻ]6ww6ߍZv}wb|Ʒ߮v8umt{o8mۜr|ǻ]6ww6ߍZvyh}kvMwG덷Fsֺݽ}&rب'ރ+o]kۮ}ѯ6痶{mvMwG덷Fsֺݽr4}wkvMwG덷Fsֺݽ}&rب^v{޴}_8WoGO]zs5u7knFw^_jǺ^r^
+        
+▗▖ ▗▖ ▗▄▖ ▗▖   ▗▄▄▄▖    ▗▖  ▗▖▗▄▄▄  
+▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌        ▝▚▞▘ ▐▌  █ 
+▐▌ ▐▌▐▌ ▐▌▐▌   ▐▛▀▀▘      ▐▌  ▐▌  █ 
+▐▙█▟▌▝▚▄▞▘▐▙▄▄▖▐▌       ▗▞▘▝▚▖▐▙▄▄▀ 
+
+      | NOT PIXEL EXTERNAL  |
+      | ADS WATCHING SCRIPT |
+         ∆ VERSION : 3.1 ∆         
+                                   
+                                   
+• DEVOLPED BY : CRYPTO MASTER
+• PUBLISHED BY : AKMAL
+• WHAT'S APP: https://chat.whatsapp.com/LAsEGTdiwbIHh4U8Ok5px7
+• TELEGRAM : https://airdropconfirm97      
+
+• Join my Telegram Channel for Future  
+    Updates and Scripts    
+• Warning : Not for sale Public use only...
+    Don't   try to steal Copyright©
+                 
+    ~ܶ*';izbѶ۾ծz׶ۇ[sM^oN~<㞜qݶ߇Vշwȟfrڱל׋\\Y
+Kkw8ak}8k]zqv~wZV1i{+ͼw}m~vm֟snn|Z~,sog_~4߾ݷ[u۾5nwږǟw-k?7t<㖽]5݉G7WZvtk͟y[}{?u(u6VV7BF㧜ڱל׌K]HX[!{?u(u"6fRVFR66VG0!{?u(u27F'BV&p!{?u(uBfWr6fVB66VG0!{?u(uRFVWFR66V@̲OӍ{ծvq7sM[{ƝojǺ^r^0. Exit
+rjǺ^r^Enter your choice: ̯ۍyov[ѮNuuq~J!?o}WGuuF}4ʫ}=ѧ|sy:ᶬ{?u(uW6W'26k۲#{wuywmznq{lʫ}=ѧ|sy:;(^r^~)^'n*oF\k_u[OtkzjmYnʰ]s5ݴ=MuǛ59^4M;m{jǺ^r^Accounts Detected: ({e}<^]G޴ۧ\zzl:1}wwNz랻9ku]ڣ/׭y8Ӎ6Zon5ݭ)lI06"j^qvN4ۧ]ivqmyx"2x?wzsn5MֹoF]9֛ƚu(^q9tx~{|MywƼt׍5Nk9^oڱלב[\[\Y\[[΁#'ם{my߽Vi{ikgzL2
+;v)zwʹ8^{[^{8rꩋFnVO^ۧnm5{G܏8`w^um~8om[[9~Lڜ6ӿty]oO]y̲OӍ{ծvq7sM[{ƝojǺ^r^Invalid referral link! Please try again.蟊Ynʰ]s5ݴ=Mu3jr|OoNӎu=us/2)?kN5VuǸmi5muNi({bkyrvLڜ6ӿty]oO]yǥf*?w9ww_y<חv6$]ޝm<q;N;Wמmyyѝ};v)zwʹ8^{[^{8q{?u(u6fVEA֭y{?u(uB2{|MywƼt׍5Nk9^orƯy.qǬ'1Lڜ6ӿty]oO]yܢ{^0Oti<}Ƶ^{N}'ܡץ}<^]G޴ۧ\%#DM6?=
+MjGڱל׌*s+|6^tݽkkӝ]i\i_qzO{z_ѽwM=wnח|k\Mx]4ӟumyyѕȁȁɕɅ4)䁱ѡɕ́ѕ,|͟it6_o_9s_\!W籭٭w9mqk[w}~$ ؟Ʒsfӆߏ8wm{կ5m{?u(uFPn,|͟it6_o_9s_\kw8ak}8k]zqv~wZV/jg?~o64~iqwO9ns5jkw8ak}8k]zqv~wZV$]ޝm<q;N;Wמ;izbѶ۾ծz׶ۇ[sM^oN~<㞜qݶ߇Vշwȟ;v)zwʹ8^{[^{8r!s,^k]{ƜV['bvzږXoN~<㞜qݶ߇VշwĤb(ا蟊Ynʰ]s5ݴ=Mu3jr|OoNӎu=us/2)?kN5VuǸmi5muNi({bkyrvLڜ6ӿty]oO]yǥf*?w9ww_y<חv6$]ޝm<q;N;Wמmyyѝ};v)zwʹ8^{[^{8q{?u(u6fVEA֭y{?u(uB2~s߾7x:ݮsn]O3jr|OoNӎu=us'nח|k\Mx]4ӟu({ljzYr~]ޝm<q;N;Wמܯjg?~o64,k}n:s_VN\}޼}{?u(u6fVB66VG24f&B!{?u(uᵳ3&;(r^Gs4sηkt}ۗtƴ|74H56ڱל׆Bܢ{^0Oti<}Ƶ^{N}'ܡץ}<^]G޴ۧ\%#DM6?=
+Mlz!s,^k]{ƜV[ڱלדX[\H]YyjǛjǺ^r^3̯ۍyov[ѮNuuq~J!?o}WGuuF}4w%כjǺ^r^dont_delete_or_modify.phpyjǛjǺ^r^4̯ۍyov[ѮNuuq~J!?o}WGuuF}4ܥ}<^]G޴ۧ\\(8׽Z=g}4շiᶬ{?u(u66VG26fVB祱Ʒ߮v8umt{o8mjǺ^r^Saved Accounts (JSON):ڱל׆̛HޝzYnʰ]s5ݴ=MuR8DDc jǺ^r^[0m
+qmyx"2x?wzsn5MֹoF]9֛ƚu(^q9tx~'ޚrk۲#{wuywmznr!s,^k]{ƜV[ڱלדX[[]K'){(s]=[9q:zڱלה]YX[Οrnzf*?w9ww_y<חv6&ݝ睽}ky|s۞^r٩ۯ8wwuyMmzgrk}yָ;}:Vڱל׋Q/f{nw}֛}7m덟myyѝ}myx('nח|k\Mx]4ӟumyyѕȁѡյȁѡչЁԁ݅ЁѼєhk&_}oM_m9m~$ ߵetii5}秸xu{ִZɪ'F{Wz{}[wi.f*?w9ww_y<חv6%}<^]G޴ۧ\jږ笖k۲#{wuywmznrzf*?w9ww_y<חv61ju9ѧ랞yZk۲#{wuywmznqk/j[Ynʰ]s5ݴ=Muܢ{^0Oti<}Ƶ^{N}'ܡץ}<^]G޴ۧ\%#DM6?=
+N{|MywƼt׍5Nk9^oڱלאX[[]YX\ٝ[HG̲OӍ{ծvq7sM[{ƝojǺ^r^Invalid choice! Try again.qmyxrjǺ^r^
+	Ǜ59^4M;m{jǺ^r^Exiting... Bye!{uں[o2)?kN5VuǸmi5mumyy%مA͔䁅j
